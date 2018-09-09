b15fa48df1f1f630c6a4d8c0feebf68f0a4d1d96
wangkai <610288425@qq.com>
2018-09-09 12:26:54 +0800

    redis test    cr

--- service/src/main/java/com/example/demo/service/RedisActService.java
+++ service/src/main/java/com/example/demo/service/RedisActService.java
@@ -1,9 +1,9 @@
-package com.example.demo.service;
-
-/**
- * @author wangkai
- * @date 2018-05-24 11:15
- * @desc com.example.demo.service
- */
-public interface RedisActService extends RedisService {
-}
+//package com.example.demo.service;
+//
+///**
+// * @author wangkai
+// * @date 2018-05-24 11:15
+// * @desc com.example.demo.service
+// */
+//public interface RedisActService extends RedisService {
+//}
--- service/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ service/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -2,10 +2,15 @@ package com.example.demo.service.impl;
 
 import com.example.demo.persist.entity.user.UserEntity;
 import com.example.demo.persist.mapper.user.UserMapper;
+import com.example.demo.redis.service.RedisService;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+
+import javax.annotation.Resource;
+
 
 /**
  * Created by wangkai on 2018/9/8.
@@ -15,9 +20,16 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Resource
+    @Qualifier(value = "redisService")
+    private RedisService redisService;
+
     @Override
     public UserEntity getUserInfoById(Integer userId) {
         Assert.notNull(userId, "Required param is null!");
+//        redisService.set("aaa", "ml");
+        String aaa = redisService.get("aaa");
+        System.out.println(aaa);
         return userMapper.selectByPrimaryKey(userId);
     }
 }
